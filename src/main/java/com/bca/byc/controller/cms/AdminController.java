49db387bdfc7a6f554c705ebba6712f697205ec9
benaleobayu <benaleobs@gmail.com>
2024-08-15 13:16:17 +0700

    update admin controller

---
+++ src/main/java/com/bca/byc/controller/cms/AdminController.java
@@ -0,0 +1,119 @@
+package com.bca.byc.controller.cms;
+
+import com.bca.byc.model.cms.InterestCategoryModelDTO;
+import com.bca.byc.model.component.Breadcrumb;
+import com.bca.byc.service.InterestCategoryService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping(AdminController.thisUrl)
+public class AdminController {
+
+    private static final String suffixName = "admin";
+    public static final String thisUrl = "/cms/settings/" + AdminController.suffixName;
+    private final String titlePage = "Admin";
+    @Autowired
+    private InterestCategoryService service;
+
+    // get route index table
+    @GetMapping
+    public String index(Model model, HttpServletRequest request) {
+        // set breadcrumb
+        List<Breadcrumb> breadcrumbs = Arrays.asList(new Breadcrumb("Home", "/", false),
+                new Breadcrumb("Master " + titlePage, request.getRequestURI(), true));
+        model.addAttribute("breadcrumbs", breadcrumbs);
+
+        //table
+        List<InterestCategoryModelDTO.DetailResponse> alldata = service.findAllData();
+        model.addAttribute("datas", alldata);
+
+        // some part
+        model.addAttribute("titlePage", titlePage);
+        model.addAttribute("modelName", suffixName);
+        return thisUrl + "/index";
+    }
+
+    // get route detail data
+    @GetMapping("/{id}/detail")
+    public String view(@PathVariable("id") Long id, Model model, HttpServletRequest request) {
+        // set breadcrumb
+        List<Breadcrumb> breadcrumbs = Arrays.asList(new Breadcrumb("Home", "/", false),
+                new Breadcrumb("Master " + titlePage, thisUrl, false),
+                new Breadcrumb("Details", request.getRequestURI(), true));
+        model.addAttribute("breadcrumbs", breadcrumbs);
+
+        InterestCategoryModelDTO.DetailResponse data = service.findDataById(id);
+        model.addAttribute("formData", data);
+        model.addAttribute("formMode", "view");
+        return thisUrl + "/form_data";
+    }
+
+    // get route create data
+    @GetMapping("/create")
+    public String create(Model model, HttpServletRequest request) {
+        // set breadcrumb
+        List<Breadcrumb> breadcrumbs = Arrays.asList(new Breadcrumb("Home", "/", false),
+                new Breadcrumb("Master " + titlePage, thisUrl, false),
+                new Breadcrumb("Create", request.getRequestURI(), true));
+        model.addAttribute("breadcrumbs", breadcrumbs);
+
+        // some part
+        InterestCategoryModelDTO.CreateRequest dto = new InterestCategoryModelDTO.CreateRequest();
+        model.addAttribute("formData", dto);
+        model.addAttribute("modelName", suffixName);
+        model.addAttribute("formMode", "create");
+        return thisUrl + "/form_data";
+    }
+
+    // get method create data
+    @PostMapping("/create")
+    public String create(@ModelAttribute("formData") @Valid InterestCategoryModelDTO.CreateRequest dto, BindingResult bindingResult, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("formData", dto);
+            return thisUrl + "/create";
+        }
+
+        service.saveData(dto);
+        return "redirect:/cms/ms/" + suffixName;
+    }
+
+    // get route edit data
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") Long id, HttpServletRequest request) {
+        // set breadcrumb
+        List<Breadcrumb> breadcrumbs = Arrays.asList(new Breadcrumb("Home", "/", false),
+                new Breadcrumb("Master " + titlePage, thisUrl, false),
+                new Breadcrumb("Edit", request.getRequestURI(), true));
+        model.addAttribute("breadcrumbs", breadcrumbs);
+
+        // some part
+        InterestCategoryModelDTO.DetailResponse dto = service.findDataById(id);
+        model.addAttribute("formData", dto);
+        model.addAttribute("formMode", "update");
+        model.addAttribute("modelName", suffixName);
+        return thisUrl + "/form_data";
+    }
+
+    // get method edit data
+    @PostMapping("/{id}/edit")
+    public String update(@PathVariable("id") Long id, @ModelAttribute("formData") @Valid InterestCategoryModelDTO.UpdateRequest dto, BindingResult bindingResult, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("formData", dto);
+            return thisUrl + "/" + id + "/edit";
+        }
+        service.updateData(id, dto);
+        return "redirect:/cms/ms/" + suffixName;
+    }
+
+
+}
--- src/main/java/com/bca/byc/entity/Admin.java
+++ src/main/java/com/bca/byc/entity/Admin.java
@@ -1,10 +1,12 @@
 package com.bca.byc.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Date;
 
 @Entity
+@Data
 @Table(name = "admins")
 public class Admin {
 
@@ -34,68 +36,4 @@ public class Admin {
     @JoinColumn(name = "role_id", referencedColumnName = "id")
     private Role role;
 
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRememberToken() {
-        return rememberToken;
-    }
-
-    public void setRememberToken(String rememberToken) {
-        this.rememberToken = rememberToken;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
