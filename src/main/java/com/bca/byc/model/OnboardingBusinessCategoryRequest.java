8f45f1db062591abfa6b7c6e9512d58e988e2a04
benaleobayu <benaleobs@gmail.com>
2024-08-23 09:59:16 +0700

    remove some files

--- src/main/java/com/bca/byc/model/OnboardingBusinessCategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bca.byc.model;
-
-import lombok.Data;
-
-@Data
-public class OnboardingBusinessCategoryRequest {
-    private Long businessCategoryId;
-    private Long businessCategoryChildId;
-}
