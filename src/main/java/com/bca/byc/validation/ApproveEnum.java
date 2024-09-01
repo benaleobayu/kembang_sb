26cbeb24a670837cbb9864d36e23a9550d0ba412
benaleobayu <benaleobs@gmail.com>
2024-09-01 23:23:34 +0700

    approveby with enum validator

---
+++ src/main/java/com/bca/byc/validation/ApproveEnum.java
@@ -0,0 +1,20 @@
+package com.bca.byc.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = ApproveEnumValidator.class)
+public @interface ApproveEnum {
+    String message() default "Invalid value must be one of: SYSTEM, ADMIN_SPV, ADMIN_OPT, SUPERADMIN";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
+
