3636e564af298e99f99ebfc33787a14159fe681a
benaleobayu <benaleobs@gmail.com>
2024-09-11 13:12:33 +0700

    seeder

--- src/main/java/com/bca/byc/seeder/AdminUserSeeder.java
+++ src/main/java/com/bca/byc/seeder/AdminUserSeeder.java
@@ -26,41 +26,41 @@ public class AdminUserSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        //create admin
-//        if (appAdminRepository.count() == 0) {
-//            AppAdmin admin = new AppAdmin();
-//            admin.setName("admin");
-//            admin.setEmail("admin@unictive.net");
-//            admin.setPassword(passwordEncoder.encode("password"));
-//            admin.setActive(true);
-//            admin.setCreatedAt(LocalDateTime.now());
-//            admin.setUpdatedAt(LocalDateTime.now());
-//
-//            // set role
-//            Role adminRole = roleRepository.findByName("SUPERADMIN")
-//                    .orElseThrow(() -> new RuntimeException("Role not found"));
-//            admin.setRole(adminRole);
-//
-//            appAdminRepository.save(admin);
-//        }
-//
-//        // create user
-//        if (appUserRepository.count() == 0) {
-//            AppUser user = new AppUser();
-//            user.setName("user");
-//            user.setEmail("user@unictive.net");
-//            user.setPassword(passwordEncoder.encode("password"));
-//            user.setActive(true);
-//            user.setCreatedAt(LocalDateTime.now());
-//            user.setUpdatedAt(LocalDateTime.now());
-//
-//            // set role
-//            Role userRole = roleRepository.findByName("USER")
-//                    .orElseThrow(() -> new RuntimeException("Role not found"));
-//            user.setRole(userRole);
-//
-//            appUserRepository.save(user);
-//        }
+        //create admin
+        if (appAdminRepository.count() == 0) {
+            AppAdmin admin = new AppAdmin();
+            admin.setName("admin");
+            admin.setEmail("admin@unictive.net");
+            admin.setPassword(passwordEncoder.encode("password"));
+            admin.setActive(true);
+            admin.setCreatedAt(LocalDateTime.now());
+            admin.setUpdatedAt(LocalDateTime.now());
+
+            // set role
+            Role adminRole = roleRepository.findByName("SUPERADMIN")
+                    .orElseThrow(() -> new RuntimeException("Role not found"));
+            admin.setRole(adminRole);
+
+            appAdminRepository.save(admin);
+        }
+
+        // create user
+        if (appUserRepository.count() == 0) {
+            AppUser user = new AppUser();
+            user.setName("user");
+            user.setEmail("user@unictive.net");
+            user.setPassword(passwordEncoder.encode("password"));
+            user.setActive(true);
+            user.setCreatedAt(LocalDateTime.now());
+            user.setUpdatedAt(LocalDateTime.now());
+
+            // set role
+            Role userRole = roleRepository.findByName("USER")
+                    .orElseThrow(() -> new RuntimeException("Role not found"));
+            user.setRole(userRole);
+
+            appUserRepository.save(user);
+        }
 
     }
 }
