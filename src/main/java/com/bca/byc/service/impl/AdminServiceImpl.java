5608cfb6fd1ce7621bee5ebed1d8b92a3c4e7602
benaleobayu <benaleobs@gmail.com>
2024-09-11 11:28:49 +0700

    fix err

--- src/main/java/com/bca/byc/service/impl/AdminServiceImpl.java
+++ src/main/java/com/bca/byc/service/impl/AdminServiceImpl.java
@@ -1,74 +1,55 @@
 package com.bca.byc.service.impl;
 
-import com.bca.byc.converter.AppSearchDTOConverter;
-import com.bca.byc.entity.AppUser;
-import com.bca.byc.entity.Post;
-import com.bca.byc.exception.BadRequestException;
-import com.bca.byc.model.AppSearchDetailResponse;
-import com.bca.byc.repository.AppSearchRepository;
-import com.bca.byc.repository.PostRepository;
-import com.bca.byc.repository.auth.AppUserRepository;
+import com.bca.byc.entity.AppAdmin;
+import com.bca.byc.model.AdminCmsDetailResponse;
+import com.bca.byc.model.AdminCreateRequest;
+import com.bca.byc.model.AdminDetailResponse;
+import com.bca.byc.model.AdminUpdateRequest;
 import com.bca.byc.response.ResultPageResponseDTO;
-import com.bca.byc.service.AppSearchService;
-import com.bca.byc.util.PaginationUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import com.bca.byc.service.AdminService;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
-public class AppSearchServiceImpl implements AppSearchService {
-
-    private final AppUserRepository appUserRepository;
-    private final AppSearchRepository repository;
-    private final PostRepository postRepository;
-    private final AppSearchDTOConverter converter;
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public AdminDetailResponse findDataById(Long id) throws Exception {
+        return null;
+    }
 
     @Override
-    public ResultPageResponseDTO<AppSearchDetailResponse> listData(String email, Integer pages, Integer limit, String sortBy, String direction, String tag, String categories) {
-        // get user id
-        AppUser user = appUserRepository.findByEmail(email)
-                .orElseThrow(() -> new BadRequestException("User not found"));
-        Long userId = user.getId();
+    public List<AdminDetailResponse> findAllData() {
+        return null;
+    }
 
-        tag = StringUtils.isEmpty(tag) ? "%" : tag + "%";
-        Sort sort = Sort.by(new Sort.Order(PaginationUtil.getSortBy(direction), sortBy));
-        Pageable pageable = PageRequest.of(pages, limit, sort);
+    @Override
+    public void saveData(AdminCreateRequest dto) throws Exception {
 
-        Page<Post> pageResult = null;
-        if ("posts".equalsIgnoreCase(categories)) {
-            pageResult = postRepository.findRandomPosts(tag, pageable);
-        } else if ("accounts".equalsIgnoreCase(categories)) {
-            pageResult = postRepository.findLatestPostsFromFollowingUsers(userId, tag, pageable);
-        } else {
-            pageResult = postRepository.findByTitleLikeIgnoreCase(tag, pageable);
-        }
+    }
 
-        assert pageResult != null;
-        List<AppSearchDetailResponse> dtos = pageResult.stream().map((c) -> {
-            AppSearchDetailResponse dto = converter.convertToListResponse(c);
-            return dto;
-        }).collect(Collectors.toList());
+    @Override
+    public void updateData(Long id, AdminUpdateRequest dto) throws Exception {
 
-        int currentPage = pageResult.getNumber() + 1;
-        int totalPages = pageResult.getTotalPages();
+    }
 
-        return PaginationUtil.createResultPageDTO(
-                pageResult.getTotalElements(), // total items
-                dtos,
-                currentPage, // current page
-                currentPage > 1 ? currentPage - 1 : 1, // prev page
-                currentPage < totalPages - 1 ? currentPage + 1 : totalPages - 1, // next page
-                1, // first page
-                totalPages - 1, // last page
-                pageResult.getSize() // per page
-        );
+    @Override
+    public void deleteData(Long id) throws Exception {
+
+    }
+
+    @Override
+    public ResultPageResponseDTO<AdminDetailResponse> listData(Integer pages, Integer limit, String sortBy, String direction, String userName) {
+        return null;
+    }
+
+    @Override
+    public AdminCmsDetailResponse getAdminDetail(String email) {
+        return null;
+    }
+
+    @Override
+    public AppAdmin findByEmail(String email) {
+        return null;
     }
 }
