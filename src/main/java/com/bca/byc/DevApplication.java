b33870b9cb3281223d1a70f16a558f62c83f4286
benaleobayu <benaleobs@gmail.com>
2024-09-12 18:06:14 +0700

    test cors

--- src/main/java/com/bca/byc/DevApplication.java
+++ src/main/java/com/bca/byc/DevApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages = "com.bca.byc")
 @EnableJpaRepositories(basePackages = "com.bca.byc.repository")
@@ -17,5 +23,20 @@ public class DevApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevApplication.class, args);
     }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "https://admin-byc2024.kelolain.id",
+                "http://localhost:4200"
+        ));
+        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        corsConfiguration.setAllowCredentials(true);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
