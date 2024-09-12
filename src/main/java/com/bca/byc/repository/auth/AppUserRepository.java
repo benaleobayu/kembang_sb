e37d7c4ec629361e2f20c6e05e429422654f3f79
benaleobayu <benaleobs@gmail.com>
2024-09-12 21:06:46 +0700

    update onboardinguser

--- src/main/java/com/bca/byc/repository/auth/AppUserRepository.java
+++ src/main/java/com/bca/byc/repository/auth/AppUserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bca.byc.entity.AppUser;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
@@ -16,11 +15,11 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     boolean existsByEmail(String email);
 
-    @Query(value = "SELECT au FROM AppUser au " +
-            "LEFT JOIN au.appUserDetail aud ON aud.id = au.appUserDetail.id " +
-            "LEFT JOIN au.appUserAttribute aua ON aua.id = au.appUserAttribute.id " +
-            "WHERE aua.isApproved = true AND aud.status = :statusType AND au.name LIKE :userName")
-    Page<AppUser> findOnboardingUser(String userName, StatusType statusType, Pageable pageable);
+//    @Query(value = "SELECT au, aud, aua FROM AppUser au " +
+//            "LEFT JOIN au.appUserDetail aud ON aud.id = au.appUserDetail.id " +
+//            "LEFT JOIN au.appUserAttribute aua ON aua.id = au.appUserAttribute.id " +
+//            "WHERE aud.status = 6")
+    Page<AppUser> findByNameLikeIgnoreCaseAndAppUserDetailStatusAndAppUserAttributeIsRecommendedTrue(String userName, StatusType statusType, Pageable pageable);
 
     Page<AppUser> findByNameLikeIgnoreCase(String name, Pageable pageable);
 
