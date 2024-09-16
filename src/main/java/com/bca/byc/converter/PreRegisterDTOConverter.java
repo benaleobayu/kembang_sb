0f0b77f9b3fd8f4a826494832603ba9847dbaea9
benaleobayu <benaleobs@gmail.com>
2024-09-16 19:57:52 +0700

    get logic status approval of created by

--- src/main/java/com/bca/byc/converter/PreRegisterDTOConverter.java
+++ src/main/java/com/bca/byc/converter/PreRegisterDTOConverter.java
@@ -1,6 +1,9 @@
 package com.bca.byc.converter;
 
+import com.bca.byc.entity.AppAdmin;
 import com.bca.byc.entity.PreRegister;
+import com.bca.byc.enums.AdminApprovalStatus;
+import com.bca.byc.enums.AdminType;
 import com.bca.byc.model.PreRegisterCreateRequest;
 import com.bca.byc.model.PreRegisterDetailResponse;
 import com.bca.byc.model.PreRegisterUpdateRequest;
@@ -26,14 +29,37 @@ public class PreRegisterDTOConverter {
         dto.setCreatedAt(Formatter.formatLocalDateTime(data.getCreatedAt()));
         dto.setUpdatedAt(Formatter.formatLocalDateTime(data.getUpdatedAt()));
 
+        // created & updated by
+        dto.setCreatedBy(data.getCreatedBy().getUsername());
+        dto.setUpdatedBy(data.getUpdatedBy().getUsername());
+
         // return
         return dto;
     }
 
     // for create data
-    public PreRegister convertToCreateRequest(@Valid PreRegisterCreateRequest dto) {
+    public PreRegister convertToCreateRequest(@Valid PreRegisterCreateRequest dto, AppAdmin admin) {
         // mapping DTO Entity with Entity
         PreRegister data = modelMapper.map(dto, PreRegister.class);
+
+        data.setCreatedBy(admin);
+
+        AdminType typeEquals = admin.getType();
+
+        switch (typeEquals) {
+            case SUPERADMIN:
+                data.setStatusApproval(AdminApprovalStatus.APPROVED);
+                break;
+            case OPERATIONAL:
+                data.setStatusApproval(AdminApprovalStatus.OPT_APPROVED);
+                break;
+            case SUPERVISOR:
+                data.setStatusApproval(AdminApprovalStatus.SPV_APPROVED);
+                break;
+            default:
+                data.setStatusApproval(AdminApprovalStatus.PENDING);
+                break;
+        }
 
         // return
         return data;
@@ -45,5 +71,8 @@ public class PreRegisterDTOConverter {
         modelMapper.map(dto, data);
         // set updated_at
         data.setUpdatedAt(LocalDateTime.now());
+
+        // set updated_by
+        data.setUpdatedBy(data.getCreatedBy());
     }
 }
