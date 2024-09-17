716482d26778928ec34c3a1590cb46e3fbadbe51
benaleobayu <benaleobs@gmail.com>
2024-09-17 16:11:53 +0800

    add public show pre register

--- src/main/java/com/bca/byc/controller/PublicController.java
+++ src/main/java/com/bca/byc/controller/PublicController.java
@@ -2,21 +2,28 @@ package com.bca.byc.controller;
 
 
 import com.bca.byc.exception.BadRequestException;
+import com.bca.byc.model.PreRegisterDetailResponse;
 import com.bca.byc.response.ApiResponse;
+import com.bca.byc.response.PaginationResponse;
+import com.bca.byc.response.ResultPageResponseDTO;
 import com.bca.byc.service.*;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.bca.byc.controller.PublicController.urlRoute;
+
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/public")
+@RequestMapping(urlRoute)
 @Tag(name = "Apps Public API", description = "Public API for public usage")
 @AllArgsConstructor
 public class PublicController {
@@ -27,6 +34,10 @@ public class PublicController {
     private final MsLocationService locationService;
     private final ExpectItemService expectItemService;
     private final ExpectCategoryService expectCategoryService;
+
+    private final PreRegisterService preRegisterService;
+
+    static final String urlRoute = "/api/v1/public";
 
     // show by identity
     @GetMapping("/setting")
@@ -81,6 +92,23 @@ public class PublicController {
             return ResponseEntity.ok(new ApiResponse(true, "Successfully found expect category", expectCategoryService.findAllData()));
         } catch (BadRequestException e) {
             return ResponseEntity.badRequest().body(new ApiResponse(false, e.getMessage(), null));
+        }
+    }
+
+    // get test pre register
+    @GetMapping("/data-pre-register")
+    public ResponseEntity<PaginationResponse<ResultPageResponseDTO<PreRegisterDetailResponse>>> listData(
+            @RequestParam(name = "pages", required = false, defaultValue = "0") Integer pages,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") Integer limit,
+            @RequestParam(name = "sortBy", required = false, defaultValue = "name") String sortBy,
+            @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction,
+            @RequestParam(name = "userName", required = false) String userName) {
+        // response true
+        log.info("GET " + urlRoute + " data-pre-register endpoint hit");
+        try {
+            return ResponseEntity.ok().body(new PaginationResponse<>(true, "Success get list pre-register", preRegisterService.listData(pages, limit, sortBy, direction, userName)));
+        } catch (ExpiredJwtException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new PaginationResponse<>(false, "Unauthorized", null));
         }
     }
 
