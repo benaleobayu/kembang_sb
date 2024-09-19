91c843a866ac31a05251f861b3eb3b30437bb972
benaleobayu <benaleobs@gmail.com>
2024-09-19 09:23:04 +0800

    update filter query

--- src/main/java/com/bca/byc/repository/UserActiveRepository.java
+++ src/main/java/com/bca/byc/repository/UserActiveRepository.java
@@ -16,18 +16,21 @@ public interface UserActiveRepository extends JpaRepository<AppUser, Long> {
     Page<AppUser> findByNameLikeIgnoreCaseAndAppUserDetailStatusAndAppUserAttributeIsSuspendedFalse(String userName, StatusType statusType, Pageable pageable);
 
     @Query("SELECT u FROM AppUser u " +
+            "JOIN AppUserDetail aud ON aud.id = u.appUserDetail.id " +
+            "JOIN AppUserAttribute aua ON aua.id = u.appUserAttribute.id " +
             "LEFT JOIN Business b ON b.user.id = u.id " +
-            "LEFT JOIN b.locations loc " +
+            "LEFT JOIN BusinessHasLocation bhl ON bhl.business.id = b.id " +
+            "LEFT JOIN Location loc ON loc.id = bhl.location.id " +
             "WHERE " +
             "(LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%') ) OR " +
             "LOWER(u.appUserDetail.name) LIKE LOWER(CONCAT('%', :keyword, '%') ) OR " +
             "LOWER(u.appUserDetail.phone) LIKE LOWER(CONCAT('%', :keyword, '%') ) OR " +
-            "LOWER(u.appUserDetail.memberBankAccount) LIKE LOWER(CONCAT('%', :keyword, '%') ) ) OR " +
-            "loc.id = :locationId AND " +
-            "u.appUserDetail.status = 6 AND " +
-            "u.appUserAttribute.isSuspended = false AND " +
-            "u.appUserAttribute.isDeleted = false AND " +
-            "u.appUserDetail.createdAt BETWEEN :startDate AND :endDate")
+            "LOWER(u.appUserDetail.memberBankAccount) LIKE LOWER(CONCAT('%', :keyword, '%') ) ) AND " +
+            "(:locationId IS NULL OR loc.id = :locationId) AND " +
+            "aud.status = 6 AND " +
+            "aua.isSuspended = false AND " +
+            "aua.isDeleted = false AND " +
+            "u.createdAt BETWEEN :startDate AND :endDate")
     Page<AppUser> findByKeywordAndStatusAndCreatedAt(@Param("keyword") String keyword,
                                                      @Param("locationId") Long locationId,
                                                      @Param("startDate") LocalDateTime start,
