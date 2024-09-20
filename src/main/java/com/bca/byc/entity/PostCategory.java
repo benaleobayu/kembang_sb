4e238e54becc63a30bbe83033a4554ad41724ae6
benaleobayu <benaleobs@gmail.com>
2024-09-20 08:11:09 +0800

    add post caategory

---
+++ src/main/java/com/bca/byc/entity/PostCategory.java
@@ -0,0 +1,30 @@
+package com.bca.byc.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "post_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostCategory extends AbstractBaseEntityNoUUID {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "icon", columnDefinition = "text")
+    private String icon;
+
+    @Column(name = "name", columnDefinition = "varchar(255) default ''")
+    private String name;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+}
