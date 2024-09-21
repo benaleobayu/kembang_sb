a3859848e8edd6e455efe6648151d9b0181f6569
benaleobayu <benaleobs@gmail.com>
2024-09-21 17:22:01 +0800

    add post content api

--- src/main/java/com/bca/byc/model/attribute/PostContentRequest.java
+++ src/main/java/com/bca/byc/model/attribute/PostContentRequest.java
@@ -1,5 +1,6 @@
 package com.bca.byc.model.attribute;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.List;
@@ -7,14 +8,22 @@ import java.util.List;
 @Data
 public class PostContentRequest {
 
+    @Schema(description = "let this be null", example = "")
     private Integer index;
 
+    @Schema(description = "let this be null", example = "")
     private String content;
 
+    @Schema(description = "let this be null", example = "")
     private String type;
 
+    @Schema(description = "tag ids person", example = "1,2,3,4")
     private List<Long> tagUserIds; // user _id
 
+    @Schema(description = "fill this for the original name", example = "abc.jpg")
     private String originalName;
+
+    @Schema(description = "for generate thumbnail from video", example = "abc.jpg")
+    private String thumbnail;
 
 }
