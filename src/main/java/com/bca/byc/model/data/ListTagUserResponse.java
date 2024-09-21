0a9face60fcbfb157705a126819b242e7ea392dc
benaleobayu <benaleobs@gmail.com>
2024-09-22 01:25:38 +0700

    update model tag user

---
+++ src/main/java/com/bca/byc/model/data/ListTagUserResponse.java
@@ -0,0 +1,18 @@
+package com.bca.byc.model.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ListTagUserResponse {
+
+    private Long id;
+    private String avatar;
+    private String name;
+
+    private String businessName;
+    private String lineOfBusiness;
+    private Boolean isPrimary;
+}
+
