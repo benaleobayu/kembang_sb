dee8250b200689d94f2f0b48bbb9fde7855ddedb
benaleobayu <benaleobs@gmail.com>
2024-09-23 14:24:19 +0700

    activated auth

--- src/main/java/com/bca/byc/controller/PermissionController.java
+++ src/main/java/com/bca/byc/controller/PermissionController.java
@@ -3,10 +3,12 @@ package com.bca.byc.controller;
 import com.bca.byc.exception.BadRequestException;
 import com.bca.byc.response.ApiResponse;
 import com.bca.byc.service.PermissionService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/cms/v1/am/permission")
 @Tag(name = "Permission API")
+@SecurityRequirement(name = "Authorization")
 public class PermissionController {
 
     private PermissionService service;
