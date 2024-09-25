6f411cb3ac71b1ec2787fb252287c2b4478e9a89
benaleobayu <benaleobs@gmail.com>
2024-09-25 10:15:20 +0700

    update model detail post id

--- src/main/java/com/bca/byc/converter/PostDTOConverter.java
+++ src/main/java/com/bca/byc/converter/PostDTOConverter.java
@@ -4,28 +4,30 @@ import com.bca.byc.entity.*;
 import com.bca.byc.model.PostCreateUpdateRequest;
 import com.bca.byc.model.PostDetailResponse;
 import com.bca.byc.model.PostHomeResponse;
-import com.bca.byc.repository.PostCategoryRepository;
-import com.bca.byc.repository.PostLocationRepository;
-import com.bca.byc.repository.TagRepository;
+import com.bca.byc.model.apps.PostOwnerResponse;
+import com.bca.byc.repository.*;
 import com.bca.byc.repository.auth.AppUserRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Component
 @RequiredArgsConstructor
 public class PostDTOConverter {
 
+    @Value("${app.base.url}")
+    private final String baseUrl;
+
     private final ModelMapper modelMapper;
 
+    private final PostRepository postRepository;
     private final TagRepository tagRepository;
-    private final AppUserRepository userRepository;
+    private final PostContentRepository postContentRepository;
     private final PostLocationRepository postLocationRepository;
     private final PostCategoryRepository postCategoryRepository;
 
@@ -33,7 +35,52 @@ public class PostDTOConverter {
     // for get data
     public PostHomeResponse convertToListResponse(Post data) {
         // mapping Entity with DTO Entity
-        PostHomeResponse dto = modelMapper.map(data, PostHomeResponse.class);
+        PostHomeResponse dto = new PostHomeResponse();
+
+        dto.setPostId(data.getId());
+        dto.setPostDescription(data.getDescription());
+
+        // set list of post content
+        List<PostHomeResponse.PostContent> posts = new ArrayList<>();
+        for (PostContent postContent : data.getPostContents()) {
+            PostHomeResponse.PostContent post = new PostHomeResponse.PostContent();
+            post.setContent(postContent.getContent());
+            post.setType(postContent.getType());
+            post.setContentId(postContent.getId());
+            post.setThumbnail(postContent.getThumbnail());
+            posts.add(post);
+        }
+        dto.setPostContentList(posts);
+
+        List<String> tags = new ArrayList<>();
+        for (Tag tag : data.getTags()) {
+            tags.add(tag.getName());
+        }
+        dto.setPostTagsList(tags);
+
+        AppUser appUser = data.getUser();
+        UserManagementConverter converter = new UserManagementConverter();
+        PostOwnerResponse owner = converter.PostOwnerResponse(
+                dto.getPostOwner() != null ? dto.getPostOwner() : new PostOwnerResponse(),
+                appUser.getId(),
+                appUser.getName(),
+                appUser.getAppUserDetail() != null && appUser.getAppUserDetail().getAvatar().startsWith("uploads/") ?
+                        baseUrl + "/" + appUser.getAppUserDetail().getAvatar() : null,
+                appUser.getBusinesses().stream()
+                        .filter(Business::getIsPrimary)
+                        .findFirst()
+                        .map(Business::getName).orElse(null),
+                appUser.getBusinesses().stream()
+                        .filter(Business::getIsPrimary)
+                        .findFirst()
+                        .map(business -> business.getBusinessCategories().stream()
+                                .findFirst()
+                                .get().getBusinessCategoryParent().getName()).orElse(null),
+                appUser.getBusinesses().stream()
+                        .filter(Business::getIsPrimary)
+                        .findFirst()
+                        .map(Business::getIsPrimary).isPresent());
+        dto.setPostOwner(owner);
         // return
         return dto;
     }
@@ -49,30 +96,32 @@ public class PostDTOConverter {
     // for create data
     public Post convertToCreateRequest(AppUser user, @Valid PostCreateUpdateRequest dto) {
         // mapping DTO Entity with Entity
-        Post data = modelMapper.map(dto, Post.class);
-        // for a new post
+        Post data = new Post();
+        data.setDescription(dto.getDescription());
         data.setId(null);
-        // set user
         data.setUser(user);
-        // set list of Tags
+
+        // Set list of Tags
         Set<Tag> tags = new HashSet<>();
-        for (String tagName : dto.getTagName()) {
-            Optional<Tag> tag = tagRepository.findByName(tagName);
-            tag.ifPresentOrElse(tags::add, () -> {
-                Tag newTag = new Tag();
-                newTag.setName(tagName);
-                newTag = tagRepository.save(newTag);
-                tags.add(newTag);
-            });
+        if (dto.getTagName() != null) {
+            for (String tagName : dto.getTagName()) {
+                Optional<Tag> tag = tagRepository.findByName(tagName);
+                tag.ifPresentOrElse(tags::add, () -> {
+                    Tag newTag = new Tag();
+                    newTag.setName(tagName);
+                    tags.add(tagRepository.save(newTag));
+                });
+            }
         }
         data.setTags(tags);
 
-        // post category
+        // Post category
         if (dto.getPostCategoryId() != null) {
             PostCategory postCategory = postCategoryRepository.findById(Long.valueOf(dto.getPostCategoryId())).orElse(null);
             data.setPostCategory(postCategory);
         }
-        // post location
+
+        // Post location
         PostLocation postLocation = postLocationRepository.findByPlaceName(dto.getPostLocation().getPlaceName());
         if (postLocation == null) {
             postLocation = new PostLocation();
@@ -84,8 +133,15 @@ public class PostDTOConverter {
             postLocation = postLocationRepository.save(postLocation);
         }
         data.setPostLocation(postLocation);
-        // return
+
+        // attribute
+        data.setIsPosted(dto.getIsPosted());
+        data.setIsCommentable(dto.getIsCommentable());
+        data.setIsShareable(dto.getIsShareable());
+        data.setIsShowLikes(dto.getIsShowLikes());
+
         data.setCreatedAt(LocalDateTime.now());
+
         return data;
     }
 
@@ -97,6 +153,7 @@ public class PostDTOConverter {
         // set updated_at
         data.setUpdatedAt(LocalDateTime.now());
     }
+
 
 
 }
