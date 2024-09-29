7aade08f1b514a6c512c57539098c0715a5420d1
benaleobayu <benaleobs@gmail.com>
2024-09-29 22:59:00 +0700

    update role authority

--- src/main/java/com/bca/byc/controller/RoleController.java
+++ src/main/java/com/bca/byc/controller/RoleController.java
@@ -16,20 +16,27 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+import static com.bca.byc.controller.RoleController.urlRoute;
+
 @Slf4j
 @RestController
 @AllArgsConstructor
-@RequestMapping("/cms/v1/am/role")
+@RequestMapping(urlRoute)
 @Tag(name = "Role API")
 @SecurityRequirement(name = "Authorization")
 public class RoleController {
 
+    static final String urlRoute = "/cms/v1/am/role";
+
     private RoleService service;
 
+    @PreAuthorize("hasAuthority('role.view')")
+    @Operation(summary = "Get List Role", description = "Get List Role")
     @GetMapping
     public ResponseEntity<PaginationAppsResponse<ResultPageResponseDTO<RoleListResponse>>> listFollowUser(
             @RequestParam(name = "pages", required = false, defaultValue = "0") Integer pages,
@@ -41,17 +48,8 @@ public class RoleController {
         return ResponseEntity.ok().body(new PaginationAppsResponse<>(true, "Success get list role", service.listData(pages, limit, sortBy, direction, keyword)));
     }
 
-    @Operation(hidden = true)
-    @GetMapping("/all")
-    public ResponseEntity<?> getAll() {
-        log.info("GET /cms/v1/am/role endpoint hit");
-        try {
-            return ResponseEntity.ok(new ApiDataResponse<>(true, "Successfully found role", service.findAllData()));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ApiResponse(false, e.getMessage()));
-        }
-    }
-
+    @PreAuthorize("hasAuthority('role.read')")
+    @Operation(summary = "Get detail Role", description = "Get detail Role")
     @GetMapping("{id}")
     public ResponseEntity<?> getById(@PathVariable("id") Long id) {
         log.info("GET /cms/v1/am/role/{id} endpoint hit");
@@ -63,6 +61,8 @@ public class RoleController {
         }
     }
 
+    @PreAuthorize("hasAuthority('role.create')")
+    @Operation(summary = "Create Role", description = "Create Role")
     @PostMapping
     public ResponseEntity<ApiResponse> create(@Valid @RequestBody RoleCreateUpdateRequest item) {
         log.info("POST /cms/v1/am/role endpoint hit");
@@ -75,6 +75,8 @@ public class RoleController {
         }
     }
 
+    @PreAuthorize("hasAuthority('role.update')")
+    @Operation(summary = "Update Role", description = "Update Role")
     @PutMapping("{id}")
     public ResponseEntity<ApiResponse> update(@PathVariable("id") Long id, @Valid @RequestBody RoleCreateUpdateRequest item) {
         log.info("PUT /cms/v1/am/role/{id} endpoint hit");
@@ -86,6 +88,8 @@ public class RoleController {
         }
     }
 
+    @PreAuthorize("hasAuthority('role.delete')")
+    @Operation(summary = "Delete Role", description = "Delete Role")
     @DeleteMapping("{id}")
     public ResponseEntity<ApiResponse> delete(@PathVariable("id") Long id) {
         log.info("DELETE /cms/v1/am/role/{id} endpoint hit");
