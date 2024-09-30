8f999120ff5b49f293a7ba6b001518038924f9d5
benaleobayu <benaleobs@gmail.com>
2024-09-30 15:39:50 +0700

    adjust location type

--- src/main/java/com/bca/byc/repository/LocationRepository.java
+++ src/main/java/com/bca/byc/repository/LocationRepository.java
@@ -1,7 +1,10 @@
 package com.bca.byc.repository;
 
 import com.bca.byc.entity.Location;
+import com.bca.byc.model.projection.IdSecureIdProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,7 @@ import java.util.Optional;
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     Optional<Location> findById(Long id);
+
+    @Query("select l from Location l where l.secureId = :id")
+    Optional<IdSecureIdProjection> findBySecureId(@Param("id") String id);
 }
