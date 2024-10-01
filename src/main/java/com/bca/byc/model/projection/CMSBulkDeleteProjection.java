b0642bc4a34303ceb0461f05278268299d6662ea
benaleobayu <benaleobs@gmail.com>
2024-10-01 10:59:41 +0700

    add interface projection bulk metod

---
+++ src/main/java/com/bca/byc/model/projection/CMSBulkDeleteProjection.java
@@ -0,0 +1,7 @@
+package com.bca.byc.model.projection;
+
+public interface CMSBulkDeleteProjection {
+     Long getId();
+     String getSecureId();
+     boolean getIsDeleted();
+}
---
+++ src/main/java/com/bca/byc/model/projection/CMSBulkSuspendProjection.java
@@ -0,0 +1,7 @@
+package com.bca.byc.model.projection;
+
+public interface CMSBulkSuspendProjection {
+    Long getId();
+    String getSecureId();
+    boolean getIsSuspended();
+}
