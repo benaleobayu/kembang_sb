0108456a0a5e17b4bdff0280804e3bb6c3603203
benaleobayu <benaleobs@gmail.com>
2024-10-03 10:10:45 +0700

    implementation interface id x secureId x AppUser projection

--- src/main/java/com/bca/byc/model/projection/IdSecureIdProjection.java
+++ src/main/java/com/bca/byc/model/projection/IdSecureIdProjection.java
@@ -1,6 +1,11 @@
 package com.bca.byc.model.projection;
 
+import com.bca.byc.entity.AppUser;
+
 public interface IdSecureIdProjection {
     Long getId();
+
     String getSecureId();
+
+    AppUser toAppUser();
 }
