19dac3fcc8bdd056132602aab06faeb8e412687d
benaleobayu <benaleobs@gmail.com>
2024-10-06 08:55:58 +0700

    update model schema likedislike post, comment, comment_reply

--- src/main/java/com/bca/byc/model/attribute/SetLikeDislikeRequest.java
+++ src/main/java/com/bca/byc/model/attribute/SetLikeDislikeRequest.java
@@ -1,8 +1,14 @@
 package com.bca.byc.model.attribute;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class SetLikeDislikeRequest {
-    private Boolean isLike;
+
+    @Schema(example = "POST | COMMENT | COMMENT_REPLY")
+    private String type;
+
+    private String targetId;
+
 }
