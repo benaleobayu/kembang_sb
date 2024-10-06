db26ef4512253a09d3ad30ce6ec14ad6573c01ad
benaleobayu <benaleobs@gmail.com>
2024-10-06 19:47:47 +0700

    update business repository

--- src/main/java/com/bca/byc/repository/BusinessRepository.java
+++ src/main/java/com/bca/byc/repository/BusinessRepository.java
@@ -6,6 +6,8 @@ import com.bca.byc.entity.BusinessCategory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -27,4 +29,11 @@ public interface BusinessRepository extends JpaRepository<Business, Long> {
 
     // Pagination method to find businesses by user ID
     Page<Business> findByUser_Id(Long userId, Pageable pageable);
+
+    @Query("SELECT b FROM Business b " +
+            "JOIN BusinessHasCategory bhc ON bhc.business.id = b.id " +
+            "JOIN BusinessCategory bc ON bc.id = bhc.business.id " +
+            "WHERE (LOWER(bc.name) LIKE LOWER(CONCAT('%', :keyword, '%') )) AND " +
+            "b.user.id = :id")
+    Page<Business> findBusinessByKeyword(@Param("id") Long id, @Param("keyword") String keyword, Pageable pageable);
 }
