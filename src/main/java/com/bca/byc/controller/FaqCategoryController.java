7798677d8284dcf1332f8110dbb4e27ced336352
benaleobayu <benaleobs@gmail.com>
2024-10-09 00:04:31 +0700

    add security faq category controller

--- src/main/java/com/bca/byc/controller/FaqCategoryController.java
+++ src/main/java/com/bca/byc/controller/FaqCategoryController.java
@@ -5,12 +5,14 @@ import com.bca.byc.model.*;
 import com.bca.byc.response.*;
 import com.bca.byc.response.ApiResponse;
 import com.bca.byc.service.FaqCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -26,6 +28,9 @@ public class FaqCategoryController {
     static final String urlRoute = "/cms/v1/ms/faq";
     private FaqCategoryService service;
 
+
+    @PreAuthorize("hasAuthority('faq.view')")
+    @Operation(summary = "Get List Faq Category", description = "Get List Faq Category")
     @GetMapping
     public ResponseEntity<PaginationCmsResponse<ResultPageResponseDTO<FaqCategoryIndexResponse>>> listDataFaqCategory(
             @RequestParam(name = "pages", required = false, defaultValue = "0") Integer pages,
@@ -38,6 +43,8 @@ public class FaqCategoryController {
         return ResponseEntity.ok().body(new PaginationCmsResponse<>(true, "Success get list faq category", service.listDataFaqCategory(pages, limit, sortBy, direction, keyword)));
     }
 
+    @PreAuthorize("hasAuthority('faq.read')")
+    @Operation(summary = "Get detail Faq Category", description = "Get detail Faq Category")
     @GetMapping("{id}")
     public ResponseEntity<?> getById(@PathVariable("id") String id) {
         log.info("GET " + urlRoute + "/{id} endpoint hit");
@@ -49,6 +56,8 @@ public class FaqCategoryController {
         }
     }
 
+    @PreAuthorize("hasAuthority('faq.create')")
+    @Operation(summary = "Create Faq Category", description = "Create Faq Category")
     @PostMapping
     public ResponseEntity<ApiResponse> create(@Valid @RequestBody FaqCategoryCreateUpdateRequest item) {
         log.info("POST " + urlRoute + " endpoint hit");
@@ -61,6 +70,8 @@ public class FaqCategoryController {
         }
     }
 
+    @PreAuthorize("hasAuthority('faq.update')")
+    @Operation(summary = "Update Faq Category", description = "Update Faq Category")
     @PutMapping("{id}")
     public ResponseEntity<ApiResponse> update(@PathVariable("id") String id, @Valid @RequestBody FaqCategoryCreateUpdateRequest item) {
         log.info("PUT " + urlRoute + "/{id} endpoint hit");
@@ -72,6 +83,8 @@ public class FaqCategoryController {
         }
     }
 
+    @PreAuthorize("hasAuthority('faq.delete')")
+    @Operation(summary = "Delete Faq Category", description = "Delete Faq Category")
     @DeleteMapping("{id}")
     public ResponseEntity<ApiResponse> delete(@PathVariable("id") String id) {
         log.info("DELETE " + urlRoute + "/{id} endpoint hit");
