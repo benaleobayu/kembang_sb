098f7f69267a5eb6d6f361da282e1cf112448e3a
benaleobayu <benaleobs@gmail.com>
2024-10-10 07:54:45 +0700

    adjust tree log user management

--- src/main/java/com/bca/byc/converter/parsing/TreeLogUserManagement.java
+++ src/main/java/com/bca/byc/converter/parsing/TreeLogUserManagement.java
@@ -1,20 +1,34 @@
 package com.bca.byc.converter.parsing;
 
+import com.bca.byc.entity.AppAdmin;
 import com.bca.byc.entity.AppUser;
 import com.bca.byc.entity.PreRegister;
 import com.bca.byc.entity.UserManagementLog;
 import com.bca.byc.enums.LogStatus;
+import com.bca.byc.model.LogUserManagementRequest;
 import com.bca.byc.repository.LogUserManagementRepository;
+
+import java.time.LocalDateTime;
 
 public class TreeLogUserManagement {
 
-    public static void logUserManagement(PreRegister preRegister, AppUser user, LogStatus logStatus, String message, LogUserManagementRepository repository) {
+    public static void logUserManagement(PreRegister preRegister,
+                                         AppUser user,
+                                         AppAdmin admin,
+                                         LogStatus status,
+                                         LogUserManagementRequest dto,
+                                         LogUserManagementRepository repository) {
 
         UserManagementLog log = new UserManagementLog();
-        log.setStatus(logStatus);
+        log.setStatus(status);
         log.setPreRegister(preRegister);
         log.setUser(user);
-        log.setMessage(message);
+        log.setMessage(dto.getType());
+        log.setType(dto.getType());
+        log.setCreatedAt(LocalDateTime.now());
+        log.setUpdatedAt(LocalDateTime.now());
+        log.setUpdatedBy(admin);
         repository.save(log);
     }
+
 }
