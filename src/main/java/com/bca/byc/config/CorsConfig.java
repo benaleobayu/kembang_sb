c95b9df9740cbbed5fd898727a92d2ca5e8cd3c1
benaleobayu <benaleobs@gmail.com>
2024-10-15 17:41:08 +0700

    Revert "test update security cors"
    This reverts commit 0946f2da85ce7f5e1e007b512402aa0fcfac2013.

--- src/main/java/com/bca/byc/config/CorsConfig.java
+++ src/main/java/com/bca/byc/config/CorsConfig.java
@@ -3,7 +3,6 @@ package com.bca.byc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -29,22 +28,5 @@ public class CorsConfig {
 //
 //        return new CorsFilter(source);
 //    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "https://admin-byc2024.kelolain.id",
-                "http://localhost:4200",
-                "http://localhost"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
 }
