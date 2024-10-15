31b2be9a51c5d8992980b32ab83cc28cd7f9a072
benaleobayu <benaleobs@gmail.com>
2024-10-16 01:13:38 +0700

    test cors implements

--- src/main/java/com/bca/byc/config/CorsConfig.java
+++ src/main/java/com/bca/byc/config/CorsConfig.java
@@ -17,16 +17,11 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(Arrays.asList(
-                "https://admin-byc2024.kelolain.id",
-                "http://localhost:4200"
-        ));
-        corsConfiguration.setAllowedMethods(Arrays.asList(
-                "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
-        ));
+        // Hanya satu origin yang diizinkan
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("https://admin-byc2024.kelolain.id"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setMaxAge(3600L); // caching preflight
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
