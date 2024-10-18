090c5010369d5df7408144b2a07e0e185eeda7e6
benaleobayu <benaleobs@gmail.com>
2024-10-18 10:40:54 +0700

    test

---
+++ src/main/java/com/bca/byc/model/projection/ReportContentIndexProjection.java
@@ -0,0 +1,35 @@
+package com.bca.byc.model.projection;
+
+import com.bca.byc.entity.Post;
+import com.bca.byc.entity.Tag;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public interface ReportContentIndexProjection {
+
+    String getId();
+
+    Long getIndex();
+
+    String getHighlight();
+
+    String getThumbnail();
+
+    String getDescription();
+
+    Set<Tag> getTags();
+
+    String getCreator();
+
+    String getReporterEmail();
+
+    Long getTotalReport();
+
+    LocalDateTime getLastReportAt();
+
+    String getChannelName();
+
+    Post getPost();
+
+}
