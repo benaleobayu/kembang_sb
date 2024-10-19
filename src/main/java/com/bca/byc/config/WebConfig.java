bfe948258e211cd14428d5ba6f3ca132be2ea042
benaleobayu <benaleobs@gmail.com>
2024-10-19 13:52:22 +0700

    test cors

--- src/main/java/com/bca/byc/config/WebConfig.java
+++ src/main/java/com/bca/byc/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.bca.byc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:/var/www/html/cms-byc2024/uploads/");
     }
 
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Atur endpoint sesuai kebutuhan
+                .allowedOrigins("https://admin-byc2024.kelolain.id")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
