10778f87c44e01f7e2fcdbf8a30ea9a78c580fb1
benaleobayu <benaleobs@gmail.com>
2024-10-23 09:22:15 +0700

    test role permission

--- src/main/java/com/bca/byc/converter/parsing/TreeRolePermissionConverter.java
+++ src/main/java/com/bca/byc/converter/parsing/TreeRolePermissionConverter.java
@@ -40,16 +40,18 @@ public class TreeRolePermissionConverter {
                 // Create a new PermissionResponse
                 PermissionResponse permissionDetail = new PermissionResponse();
 
+                boolean disabled = !matchingPermission.isPresent() || matchingPermission.get().getPermission().getId() == null;
+
                 boolean isActive = matchingPermission.isPresent();
                 if (matchingPermission.isPresent()) {
                     permissionDetail.setPermissionId(matchingPermission.get().getPermission().getId());
                     permissionDetail.setPermissionName(defaultPermission);
-                    permissionDetail.setDisabled(false); // permission exists, not disabled
+                    permissionDetail.setDisabled(disabled); // permission exists, not disabled
                     permissionDetail.setActive(isActive);
                 } else {
                     permissionDetail.setPermissionId(null); // no ID because it doesn't exist
                     permissionDetail.setPermissionName(defaultPermission);
-                    permissionDetail.setDisabled(true); // permission doesn't exist, disabled
+                    permissionDetail.setDisabled(disabled); // permission doesn't exist, disabled
                     permissionDetail.setActive(false);
                 }
 
