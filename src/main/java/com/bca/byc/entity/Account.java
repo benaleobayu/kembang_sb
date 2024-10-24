87a534bb927f6fb4d56cde8178529d5e2c890825
benaleobayu <benaleobs@gmail.com>
2024-10-24 16:33:26 +0700

    update the account entity

--- src/main/java/com/bca/byc/entity/Account.java
+++ src/main/java/com/bca/byc/entity/Account.java
@@ -17,6 +17,12 @@ public class Account extends AbstractBaseEntityCms implements SecureIdentifiable
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "avatar", columnDefinition = "text")
+    private String avatar;
+
+    @Column(name = "cover", columnDefinition = "text")
+    private String cover;
+
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<AccountHasChannels> accountHasChannels = new ArrayList<>();
 
