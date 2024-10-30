f737c86eb613e78c8de83a2c2d7745c0d21d92ed
benaleobayu <benaleobs@gmail.com>
2024-10-30 14:20:24 +0700

    add : new privilege on broadcast

--- src/main/java/com/bca/byc/seeder/RoleSeeder.java
+++ src/main/java/com/bca/byc/seeder/RoleSeeder.java
@@ -63,6 +63,9 @@ public class RoleSeeder implements CommandLineRunner {
         actionByResource.put("request_contact", List.of("view", "read", "update", "delete"));
         actionByResource.put("accounts", List.of("view", "create" , "read","update", "delete", "export"));
 
+        // inbox
+        actionByResource.put("accounts", List.of("view", "create" , "read","update", "delete", "export"));
+
 
         List<Permission> allPermissions = new ArrayList<>();
 
