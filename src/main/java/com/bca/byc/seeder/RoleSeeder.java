7819a1f61a70f1e581e3935ffa36be0a0700b544
benaleobayu <benaleobs@gmail.com>
2024-10-31 23:37:22 +0700

    add : role permission for blacklist, location, branch with 'import'

--- src/main/java/com/bca/byc/seeder/RoleSeeder.java
+++ src/main/java/com/bca/byc/seeder/RoleSeeder.java
@@ -59,6 +59,7 @@ public class RoleSeeder implements CommandLineRunner {
         actionByResource.put("tag", List.of("view", "create", "read", "update", "delete"));
         actionByResource.put("reason_report", List.of("view", "create", "read", "update", "delete"));
         actionByResource.put("blacklist_keyword", List.of("view", "create", "read", "update", "delete", "export", "import"));
+        actionByResource.put("document", List.of("view", "create", "read", "update", "delete"));
         actionByResource.put("setting", List.of("view", "create", "read", "update", "delete"));
 
         actionByResource.put("request_contact", List.of("view", "read", "update", "delete"));
